a9c43edb3e5f525ff002f8d26ce744c2350828f4
HyeonJin Jo <hyeonjinmirim@e-mirim.hs.kr>
2017-04-06 20:35:06 +0900

    #8 버튼 여러개를 onClick()에서 모두 처리하게 하기

--- app/src/main/java/kr/hs/emirim/hyeonjinmirim/friendsimulator/a_room/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/hyeonjinmirim/friendsimulator/a_room/MainActivity.java
@@ -1,7 +1,8 @@
 package kr.hs.emirim.hyeonjinmirim.friendsimulator.a_room;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +13,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    void onClick(int view ){
-        Toast.makeText(this,"작동 해요~", Toast.LENGTH_SHORT).show();
+    void onClick(View view ){
+        switch(view.getId()){
+            case R.id.button1 :
+                Toast.makeText(this,"머임마", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2 :
+                Toast.makeText(this,"노잼", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3 :
+                Toast.makeText(this,"나 니혼진이야", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button4 :
+                Toast.makeText(this,"다이죠부해", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button5 :
+                Toast.makeText(this,"나 영어 못해서 일본어 해", Toast.LENGTH_SHORT).show();
+                break;
+            default :
+                Toast.makeText(this,"무엇을 원하는 지 모르겠어요", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        if(view.getId()==R.id.button1){
+
+        }
+
+
     }
 
 }
