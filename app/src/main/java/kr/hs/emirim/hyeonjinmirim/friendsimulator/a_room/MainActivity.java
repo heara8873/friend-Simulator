5592cb14c01d2963aeda4ab31603b1d4a50dcd47
HyeonJin Jo <hyeonjinmirim@e-mirim.hs.kr>
2017-04-06 20:16:48 +0900

    #7 첫번째 버튼 동작하게 하기)

--- app/src/main/java/kr/hs/emirim/hyeonjinmirim/friendsimulator/a_room/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/hyeonjinmirim/friendsimulator/a_room/MainActivity.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.hyeonjinmirim.friendsimulator.a_room;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    void onClick(int view ){
+        Toast.makeText(this,"작동 해요~", Toast.LENGTH_SHORT).show();
+    }
+
 }
